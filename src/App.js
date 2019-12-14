6c56bf48994248d4376c9a5d175c45554b713f3b
dimasyoga <dimasyogapra@gmail.com>
2019-12-14 13:48:35 +0700

    add button and modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import * as firebase from 'firebase/app'
+import 'firebase/firebase-database'
+import Charts from './Charts'
+import Popup from 'reactjs-popup'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var firebaseConfig = {
+  apiKey: "AIzaSyDzlWi_GqFWdzNmYUdl-4Fj2LrEIvLeDD4",
+  authDomain: "reminapp.firebaseapp.com",
+  databaseURL: "https://reminapp.firebaseio.com",
+  projectId: "reminapp",
+  storageBucket: "reminapp.appspot.com",
+  messagingSenderId: "703294009454",
+  appId: "1:703294009454:web:8facc4eec8f1c28b6e767f",
+  measurementId: "G-QPTJYLTRHC"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData1:[],
+      chartData2:[]
+    }
+  }
+  // async function
+  // componentDidMount(){
+  //   let record = firebase.database().ref('timestamped_measures');
+  //   record.once('value').then((result) => {
+  //     console.log("async: ", result.val())
+  //     this.setState({chartData:Object.values(result.val())})
+  //   }).catch((err) => {
+  //     console.log("error")
+  //   });
+  // }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    const rootRef = firebase.database().ref('timestamped_measures');
+    rootRef.on('value', snap => {
+      this.setState({ 
+        chartData1: Object.values(snap.val()['tong 1']),
+        chartData2: Object.values(snap.val()['tong 2'])
+      }, () => {
+        console.log("getData success")
+      });
+    });
+  }
+
+  _handleChoice(theChosenOne) {
+    this.getData(theChosenOne);
+  }
+  
+
+  render(){
+    return (
+      <div>
+        <div>
+          <Popup
+            trigger={<button class="btn"><i class="fa fa-trash"></i> Thrash 1 </button>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="header">Thrash 1</div>
+            <div className="content">
+              <Charts data={this.state.chartData1} />
+            </div>
+          </Popup>
+        </div>
+        <div>
+          <Popup
+            trigger={<button class="btn"><i class="fa fa-trash"></i> Thrash 2 </button>}
+            modal
+            closeOnDocumentClick
+          >
+            <div className="header">Thrash 2</div>
+            <div className="content">
+              <Charts data={this.state.chartData2} />
+            </div>
+          </Popup>
+        </div>
+      </div>
+      
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Charts.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label
+} from 'recharts';
+
+export default function charts(props) {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <AreaChart
+        width={600}
+        height={400}
+        data={props.data}
+        margin={{
+          top: 0, right: 0, left: 20, bottom: 10,
+        }}
+      >
+        <CartesianGrid strokeDasharray="1 1" />
+        <XAxis dataKey="timestamp"/>
+        <YAxis/>
+        <Tooltip />
+        <Legend />
+        <Area type="monotone" dataKey="value" stroke="#000000" fill="#000000" activeDot={{ r: 4 }} />
+        {/* <Area type="monotone" dataKey="uv" stroke="#82ca9d" fill="#82ca9d"/>
+        <Area type="monotone" dataKey="amt" stroke="#46ca9f" fill="#46ca9f"/> */}
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+}
